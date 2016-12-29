13aaff45e88d4d3c85e0db2ed79dc12040e3ea4f
Jian <wj_hust08@163.com>
2016-12-29 21:47:06 +0800

    aa

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,23 +1,56 @@
 <template>
-    <div class="demo">
-        <img src="static/logo.png" />
-        Zhao Lab
-    </div>
+  <div class="demo">
+    <table>
+      <tr>
+        <td>
+          <img :src="'static/'+top.logo" />
+        </td>
+        <td>
+          <span class="top-title">{{top.title}}</span><br><span class="top-intro">{{top.intro}}</span>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-    export default {
-
+  export default {
+    data() {
+      return {
+        top: []
+      }
+    },
+    created() {
+      this.fetch_data()
+    },
+    watch: {
+      '$route': 'fetch_data'
+    },
+    methods: {
+      fetch_data() {
+        var v = this
+        this.$http.get('static/top.json').then((response) => {
+          v.top = response.body
+        }, (response) => {
+        })
+      }
     }
+  }
 </script>
 
 <style>
 .demo {
-    text-align: left;
-    height: 50px;
-    padding: 10px 0px;
+  text-align: left;
+  height: 50px;
+  padding: 10px 0px;
 }
 .demo img {
   height: 50px;
+}
+.top-title {
+    font-size: 110%;
+}
+.top-intro {
+    font-size: 80%;
 }
 </style>
