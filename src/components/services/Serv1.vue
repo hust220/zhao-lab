84a6b532915d66ba80197b20b52840eb3c760ef3
wangjian <wj_hust08@hust.edu.cn>
2017-01-12 09:47:48 +0800

    2017-1-12

--- src/components/services/Serv1.vue
+++ src/components/services/Serv1.vue
@@ -1,14 +1,69 @@
 <template>
-    <el-card>
-        <el-row>
-            <el-col :span="24">
-                Serv1
-            </el-col>
-        </el-row>
-    </el-card>
+  <el-card>
+    <el-row>
+      <el-col :span="24">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="Input1">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="Input2">
+            <el-select v-model="form.region" placeholder="">
+              <el-option label="" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="即时配送">
+            <el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
+          </el-form-item>
+          <el-form-item label="活动性质">
+            <el-checkbox-group v-model="form.type">
+              <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+              <el-checkbox label="地推活动" name="type"></el-checkbox>
+              <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+              <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+          <el-form-item label="特殊资源">
+            <el-radio-group v-model="form.resource">
+              <el-radio label="线上品牌商赞助"></el-radio>
+              <el-radio label="线下场地免费"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="活动形式">
+            <el-input type="textarea" v-model="form.desc"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
+
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
-    export default {
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!')
+      }
     }
+  }
+
 </script>
