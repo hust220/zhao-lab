351e51e0b9f353c3f61a3774840a2120752704d7
wangjian <wj_hust08@hust.edu.cn>
2017-01-12 10:44:49 +0800

    2017-1-12

--- src/components/services/Serv1.vue
+++ src/components/services/Serv1.vue
@@ -1,40 +1,51 @@
 <template>
-  <el-card>
+  <el-card class="serv1">
+    <div slot="header" class="clearfix">
+      <span>New Serv1 Task</span>
+    </div>
     <el-row>
       <el-col :span="24">
-        <el-form ref="form" :model="form" label-width="80px">
+        <el-form ref="form" :model="form" label-width="150px">
           <el-form-item label="Input1">
-            <el-input v-model="form.name"></el-input>
+            <el-col :span="5">
+              <el-input v-model="form.name"></el-input>
+            </el-col>
           </el-form-item>
-          <el-form-item label="Input2">
-            <el-select v-model="form.region" placeholder="">
-              <el-option label="" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+          <el-form-item label="Select">
+            <el-select v-model="form.region" placeholder="Please select">
+              <el-option label="Option1" value="option1"></el-option>
+              <el-option label="Option2" value="option2"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="即时配送">
+          <el-form-item label="Switch">
             <el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
           </el-form-item>
-          <el-form-item label="活动性质">
+          <el-form-item label="Checkbox">
             <el-checkbox-group v-model="form.type">
-              <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-              <el-checkbox label="地推活动" name="type"></el-checkbox>
-              <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-              <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+              <el-checkbox label="checkbox1" name="type"></el-checkbox>
+              <el-checkbox label="checkbox2" name="type"></el-checkbox>
+              <el-checkbox label="checkbox3" name="type"></el-checkbox>
+              <el-checkbox label="checkbox4" name="type"></el-checkbox>
             </el-checkbox-group>
           </el-form-item>
-          <el-form-item label="特殊资源">
+          <el-form-item label="Radio">
             <el-radio-group v-model="form.resource">
-              <el-radio label="线上品牌商赞助"></el-radio>
-              <el-radio label="线下场地免费"></el-radio>
+              <el-radio label="radio1"></el-radio>
+              <el-radio label="radio2"></el-radio>
             </el-radio-group>
           </el-form-item>
-          <el-form-item label="活动形式">
+          <el-form-item label="Textarea">
             <el-input type="textarea" v-model="form.desc"></el-input>
+            <el-upload
+              action="//jsonplaceholder.typicode.com/posts/"
+              :on-success="handleSuccess">
+              <el-button size="small" type="primary">Upload</el-button>
+              <div class="el-upload__tip" slot="tip">Upload the file</div>
+            </el-upload>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button>取消</el-button>
+            <el-button type="primary" @click="onSubmit">Submit</el-button>
+            <el-button>Cancel</el-button>
           </el-form-item>
         </el-form>
 
@@ -49,21 +60,33 @@
       return {
         form: {
           name: '',
-          region: '',
+          region: 'option1',
           date1: '',
           date2: '',
           delivery: false,
           type: [],
           resource: '',
-          desc: ''
+          desc: '',
+          fileList: []
         }
       }
     },
     methods: {
       onSubmit() {
         console.log('submit!')
+      },
+      handleSuccess(response, file, fileList) {
+        this.form.fileList.push(file.name)
+        console.log(this.form.fileList)
       }
     }
   }
 
 </script>
+
+<style>
+  .serv1 {
+    margin: 10px 0px;
+  }
+</style>
+
--- src/components/services/Serv2.vue
+++ src/components/services/Serv2.vue
@@ -1,14 +1,94 @@
 <template>
-    <el-card>
-        <el-row>
-            <el-col :span="24">
-                Serv2
+  <el-card class="serv2">
+    <div slot="header" class="clearfix">
+      <span>New Serv2 Task</span>
+    </div>
+    <el-row>
+      <el-col :span="24">
+        <el-form ref="form" :model="form" label-width="150px">
+          <el-form-item label="Input1">
+            <el-col :span="5">
+              <el-input v-model="form.name"></el-input>
             </el-col>
-        </el-row>
-    </el-card>
+          </el-form-item>
+          <el-form-item label="Select">
+            <el-select v-model="form.region" placeholder="Please select">
+              <el-option label="Option1" value="option1"></el-option>
+              <el-option label="Option2" value="option2"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Switch">
+            <el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
+          </el-form-item>
+          <el-form-item label="Checkbox">
+            <el-checkbox-group v-model="form.type">
+              <el-checkbox label="checkbox1" name="type"></el-checkbox>
+              <el-checkbox label="checkbox2" name="type"></el-checkbox>
+              <el-checkbox label="checkbox3" name="type"></el-checkbox>
+              <el-checkbox label="checkbox4" name="type"></el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+          <el-form-item label="Radio">
+            <el-radio-group v-model="form.resource">
+              <el-radio label="radio1"></el-radio>
+              <el-radio label="radio2"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="Textarea">
+            <el-input type="textarea" v-model="form.desc"></el-input>
+            <el-upload
+              action="//jsonplaceholder.typicode.com/posts/"
+              :on-success="handleSuccess">
+              <el-button size="small" type="primary">Upload</el-button>
+              <div class="el-upload__tip" slot="tip">Upload the file</div>
+            </el-upload>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">Submit</el-button>
+            <el-button>Cancel</el-button>
+          </el-form-item>
+        </el-form>
+
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
-    export default {
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: 'option1',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: '',
+          fileList: []
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!')
+      },
+      handleSuccess(response, file, fileList) {
+        this.form.fileList.push(file.name)
+        console.log(this.form.fileList)
+      }
     }
+  }
+
 </script>
+
+
+<style>
+  .serv2 {
+    margin: 10px 0px;
+  }
+</style>
+
+
