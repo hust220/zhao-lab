9389014a65962d3400ef77b9d2498a91c6fd2d02
Jian Wang <wj_hust08@163.com>
2017-05-28 13:28:12 +0800

    aa

---
+++ src/components/Services.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-card class="services">
+        <el-row>
+            <el-col :span="24">
+                Services
+            </el-col>
+        </el-row>
+    </el-card>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+  .services {
+    margin: 10px 0px;
+  }
+</style>
