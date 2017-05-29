4ab2261264e3fc0208deae6b70623fd97511017d
Jian Wang <wj_hust08@163.com>
2017-05-29 13:10:46 +0800

    aa

--- scripts/dynamical-contact.sh
+++
@@ -1,9 +0,0 @@
-id=$1
-tasks_dir=/home/zyj/web/tasks
-n_heavy_atoms=$(cat ${tasks_dir}/${id}/n_heavy_atoms)
-n_ca_atoms=$(cat ${tasks_dir}/${id}/n_ca_atoms)
-n_frames=$(cat ${tasks_dir}/${id}/n_frames)
-cd /home/zyj/Zhao-Lab-service/Dynamical-contact
-echo ${tasks_dir}/${id}/pdb_file | bash input-generator.sh
-./distance ${n_heavy_atoms} ${n_ca_atoms} ${n_frames} >${tasks_dir}/${id}/result
-
--- scripts/structure-distance.sh
+++
@@ -1,9 +0,0 @@
-id=$1
-
-tasks_dir=/home/zyj/web/tasks
-
-cd /home/zyj/Zhao-Lab-service/structure-distance
-
-./distance ${tasks_dir}/$id/pdb_file >${tasks_dir}/$id/result
-
-cd -
